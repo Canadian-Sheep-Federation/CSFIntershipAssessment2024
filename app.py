f02975e76105adda525c99bee6d6d0a0deef391e
CianNW <cian.waterhouse@gmail.com>
2024-05-23 00:11:50 -0230

    jkbj

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request,render_template
+from flask import Flask, request, render_template, redirect, url_for
 import pickle
 import requests
 
 app = Flask(__name__)
 
-
+# Calls the Public Quiz API to compile questions returns JSON
 def quiz_builder(category, q_type, difficulty):
     cat_url = r'https://opentdb.com/api_category.php'
     base = r'https://opentdb.com/api.php?amount=10'
@@ -15,75 +15,86 @@ def quiz_builder(category, q_type, difficulty):
         for c in cat:
             if c['name'] == category:
                 category = c['id']
-                id =category
+                id = category
         base += f'&category={category}'
     if difficulty != 'Any Difficulty':
-        base += f'&difficulty={difficulty}'
+        base += f'&difficulty={difficulty.lower()}'
     if q_type != 'Any Type':
-        base += f'&type={q_type}'
+        if q_type == 'Multiple Choice':
+            q_type = 'multiple'
+        else:
+            q_type = 'boolean'
+        base += f'&type={q_type.lower()}'
     response = requests.get(base)
     return response.json()
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return open('templates/index.html')
+def home():
+    return render_template('index.html')
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    # get the data from the request
-
     name = request.form.get('name')
     category = request.form.get('trivia_category')
     q_type = request.form.get('question_type')
     difficulty = request.form.get('difficulty')
+
     try:
         dct = pickle.load(open('model.pkl', 'rb'))
         dct[len(dct) + 1] = {'name': name, 'category': category, 'q_type': q_type, 'difficulty': difficulty}
-        print(dct)
         pickle.dump(dct, open('model.pkl', 'wb'))
     except FileNotFoundError:
         dct = {1: {'name': name, 'category': category, 'q_type': q_type, 'difficulty': difficulty}}
         pickle.dump(dct, open('model.pkl', 'wb'))
-    qs = quiz_builder(category, q_type, difficulty)
-    qs = qs['results']
-    q = qs[0]
-    ques = q['question']
-    ans = q['correct_answer']
-    choices = q['incorrect_answers']
-    choices.append(ans)
-    tp = q['type']
-    qs.pop(0)
-    if tp == 'multiple':
-        game = open('templates/game.html')
-        render_template(game,  category=category, q_type=q_type, difficulty=difficulty, ques=ques, ans=ans,)
-    else:
-        game = open('templates/game2.html')
 
+    qs = quiz_builder(category, q_type, difficulty)['results']
+    return render_template('game.html', name=name, category=category, q_type=q_type, difficulty=difficulty,
+                           questions=qs, index=0, feedback=None)
 
-
-    game = open('templates/game.html')
-    return render_template(game, name=name, category=category, q_type=q_type, difficulty=difficulty)
-
-
-@app.route('/get/<id>', methods=['GET'])
-def get(id):
+@app.route('/get', defaults={'index': None})
+@app.route('/get/<int:index>')
+def get(index):
     try:
         dct = pickle.load(open('model.pkl', 'rb'))
-        return dct[int(id)]
     except FileNotFoundError:
-        return 'No data found'
+        if index is None:
+            return "No entries are in the database."
+        else:
+            return f"Less than {index} entries are in the database."
 
-
-@app.route('/get', methods=['GET'])
-def get_all():
-    try:
-        dct = pickle.load(open('model.pkl', 'rb'))
+    if index is None:
         return dct
-    except FileNotFoundError:
-        return 'No data found'
+    elif index in dct:
+        return dct[index]
+    else:
+        return f"No entry found for index {index}."
+@app.route('/game/<int:index>', methods=['POST'])
+def game(index):
+    questions = eval(
+        request.form.get('questions'))  # This is generally not recommended, use safer methods in production.
+    name = request.form.get('name')
+    category = request.form.get('category')
+    q_type = request.form.get('q_type')
+    difficulty = request.form.get('difficulty')
+    user_answer = request.form.get('answer')
+    correct_answer = request.form.get('correct_answer')
+
+    if request.form.get('feedback') == '1':
+        is_correct = user_answer == correct_answer
+        feedback = 'Correct!' if is_correct else f'Wrong! The correct answer was: {correct_answer}'
+        return render_template('game.html', name=name, category=category, q_type=q_type, difficulty=difficulty,
+                               questions=questions, index=index, feedback=feedback)
+
+    if index + 1 < len(questions):
+        next_index = index + 1
+    else:
+        return render_template('end.html', feedback=None, correct=None, questions=questions, index=index)
+
+    return render_template('game.html', name=name, category=category, q_type=q_type, difficulty=difficulty,
+                           questions=questions, index=next_index, feedback=None)
 
 
 if __name__ == '__main__':
-    quiz_builder('Science: Computers', 'multiple', 'easy')
+    app.run(debug=True)
