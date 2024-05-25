c44110410141e0aac57fd7dd8e53c7d97439114f
jimwei <jimwei6@gmail.com>
2024-05-24 23:31:54 -0700

    created server with mock api endpoints

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+// POST: Takes in the form and stores it in your chosen data store, return the id of the newly created form response
+app.post('/', (req, res) => {
+  res.status(201);
+});
+
+// GET: Returns all responses to the form
+app.get('/', (req, res) => {
+  res.status(200);
+});
+
+// GET: Returns the form corresponding to the id
+app.get('/:id', (req, res) => {
+  res.status(200);
+});
+
+// start server on PORT
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(`Server has successfully started on port ${PORT}`);
+  else
+    console.log(`Server init error: ${error}`)
+});
